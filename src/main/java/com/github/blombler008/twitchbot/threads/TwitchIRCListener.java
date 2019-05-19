39b285f21b07c89faf69a4489963d8c55ff18754
blombler008 <37677225+blombler008@users.noreply.github.com>
2019-05-19 02:53:54 +0200

    Fixed dice message not shown

--- src/main/java/com/github/blombler008/twitchbot/threads/TwitchIRCListener.java
+++ src/main/java/com/github/blombler008/twitchbot/threads/TwitchIRCListener.java
@@ -95,7 +95,7 @@ public class TwitchIRCListener extends Thread {
                                 for(Command cmd: commands) {
                                     if(got[4].equalsIgnoreCase(cmd.getCommandName())) {
                                         String outCome = cmd.run(got, this);
-                                        /*
+
                                         if(outCome != null) {
                                             String string = Strings.MSG_TEMPLATE;
                                             string = string.replaceAll("%channel%", got[3]);
@@ -103,7 +103,7 @@ public class TwitchIRCListener extends Thread {
                                             // Thread.sleep(3000);
                                             send(string);
                                         }
-                                        */
+
                                     }
                                 }
                             }
